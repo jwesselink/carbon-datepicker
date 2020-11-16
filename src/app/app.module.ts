b76b5cce657443e44f13c2f20289d75295f624ff
Jasper Wesselink <jwesselink@gmail.com>
2020-11-16 18:09:32 +0100

    Create test app for datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {DatePickerModule} from 'carbon-components-angular';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DatePickerModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
